46b9f9832a4535e0a8ebaa50186994407a8420a3
ganesh1172 <ganesh152595@gmail.com>
2020-04-20 19:23:23 +0530

    Created new functions

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,11 +2,54 @@ import React, { Component } from "react";
 import "./App.css";
 import "./bootstrap.min.css";
 
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Turn({author, books}) {
+  return (){
+    <div className="row turn" style={{backgroundColor : "white"}}>
+      <div className="col-4">
+      <img src={author.imageUrl} className="authorimage" alt="Author"/>
+    </div>
+    <div className="col-6">
+      {books.map((title) => <p>{title}</p>)}
+    </div>
+    </div>
+  }
+}
+
+function Continue() {
+  return <div />;
+}
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credt">
+          All images are from <a href="www.google.com">Wikemedia Commons </a>and
+          in the public domain
+        </p>
+      </div>
+    </div>
+  );
+}
 export default class AuthorQuiz extends Component {
   render() {
     return (
-      <div>
-        <h1 className="alert-danger">Hello</h1>
+      <div className="container-fluid">
+        <Hero />
+        <Turn />
+        <Continue />
+        <Footer />
       </div>
     );
   }
