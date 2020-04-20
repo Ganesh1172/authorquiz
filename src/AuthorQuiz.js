2df040c88b02d3f6edc5f467fd8c3178d533c628
ganesh1172 <ganesh152595@gmail.com>
2020-04-20 17:43:29 +0530

    Adding bootstrap style file and some changes

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import "./bootstrap.min.css";
+
+export default class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="alert-danger">Hello</h1>
+      </div>
+    );
+  }
+}
