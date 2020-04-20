5ab243e51e04fef691c363f9f9420a08527b9f69
ganesh1172 <ganesh152595@gmail.com>
2020-04-20 23:08:43 +0530

    Updates

---
+++ precache-manifest.17a7f6a760e2bd086153dd7215373dd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ae54d1e587d14548dc2fbd94e4cd7e",
+    "url": "/authorquiz/index.html"
+  },
+  {
+    "revision": "da8160c42b19285a9b28",
+    "url": "/authorquiz/static/css/main.961f0715.chunk.css"
+  },
+  {
+    "revision": "625f944daf7015018638",
+    "url": "/authorquiz/static/js/2.3ff273f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/authorquiz/static/js/2.3ff273f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8160c42b19285a9b28",
+    "url": "/authorquiz/static/js/main.66a40b93.chunk.js"
+  },
+  {
+    "revision": "596badb32e21f2ab4f24",
+    "url": "/authorquiz/static/js/runtime-main.dc93a066.js"
+  }
+]);
