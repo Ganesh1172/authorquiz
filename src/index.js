ff4c3c4836f0fafd0c85e19ccba1753d65b96ada
ganesh1172 <ganesh152595@gmail.com>
2020-04-20 20:12:37 +0530

    creating new function for shuffling authors information

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AuthorQuiz from "./AuthorQuiz";
+import { shuffle, sample } from "underscore";
 
 const authors = [
   {
@@ -14,12 +15,53 @@ const authors = [
       "Wings of Fire",
     ],
   },
+  {
+    name: "Mark Twain",
+    imageUrl: "images/pic.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The Advantures of Huckleberry Finn",
+      "Harry Potter",
+      "Wings of Fire",
+    ],
+  },
+  {
+    name: "Mark Twain",
+    imageUrl: "images/pic.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The Advantures of Huckleberry Finn",
+      "Harry Potter",
+      "Wings of Fire",
+    ],
+  },
+  {
+    name: "Mark Twain",
+    imageUrl: "images/pic.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The Advantures of Huckleberry Finn",
+      "Harry Potter",
+      "Wings of Fire",
+    ],
+  },
 ];
 
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+      author.books.some((title) => title === answer)
+    ),
+  };
+}
 const state = {
-  turnData: {
-    author: authors[0],
-    books: authors[0].books,
-  },
+  turnData: getTurnData(authors),
 };
 ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
