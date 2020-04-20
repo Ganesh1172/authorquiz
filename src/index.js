c4931b00a0f9df94827f2c50ccc7d783063b0ca0
ganesh1172 <ganesh152595@gmail.com>
2020-04-20 19:54:24 +0530

    Adding some author informations

--- src/index.js
+++ src/index.js
@@ -3,4 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AuthorQuiz from "./AuthorQuiz";
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById("root"));
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/pic.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The Advantures of Huckleberry Finn",
+      "Harry Potter",
+      "Wings of Fire",
+    ],
+  },
+];
+
+const state = {
+  turnData: {
+    author: authors[0],
+    books: authors[0].books,
+  },
+};
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
